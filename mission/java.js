2d49a73703ffcb304a0bfe8fc26e4145776373bc
orchard-grant <122390755+orchard-grant@users.noreply.github.com>
2025-12-08 17:33:18 -0700

    newchanges

--- mission/java.js
+++ mission/java.js
@@ -7,7 +7,9 @@ function changeTheme() {
 
   if (current === "dark") {
     document.body.classList.add("dark");
+    logo.src = "missiondark.png";
   } else {
     document.body.classList.remove("dark");
+    logo.src = "mission.webp";
   }
 }
